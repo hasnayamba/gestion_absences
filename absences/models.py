3e31a36b37974865ed4c4231ba380bf7fbb2f5a4
hasnayamba <49293915+hasnayamba@users.noreply.github.com>
2025-07-14 16:22:23 +0100

    Update models.py

--- absences/models.py
+++ absences/models.py
@@ -8,6 +8,8 @@ from django.db.models import Sum
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db.models import Sum
+from django.utils.translation import gettext_lazy as _
+
 
 # -------------------------
 # GESTIONNAIRE UTILISATEUR
