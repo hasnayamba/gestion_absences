1427f0f0ade9c9bf6a1a9e112823753d9066e784
Hassana Ibrahim Yamba <yambahassana@gmail.com>
2025-07-14 09:40:17 +0100

    added collectstatic & deplo up

--- gestion_absences/deployment.py
+++ gestion_absences/deployment.py
@@ -2,7 +2,8 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-SECRET_KEY = os.environ['SECRET']
+
+SECRET_KEY_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = [f"https://{os.environ['WEBSITE_HOSTNAME']}"]
 DEBUG = False
