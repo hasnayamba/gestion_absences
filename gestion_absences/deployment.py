b0867ffb616634b07e94853b580ba56938f8caa5
Hassana Ibrahim Yamba <yambahassana@gmail.com>
2025-07-14 09:25:44 +0100

    added collectstatic & yaml

--- gestion_absences/deployment.py
+++ gestion_absences/deployment.py
@@ -4,7 +4,7 @@ from .settings import BASE_DIR
 
 SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = [f'https://' + os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = [f"https://{os.environ['WEBSITE_HOSTNAME']}"]
 DEBUG = False
 
 MIDDLEWARE = [
@@ -20,9 +20,10 @@ MIDDLEWARE = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters = {pair.split('='): pair.split('=')[1] for pair in connection_string.split(' ')}
+parameters = dict(pair.split('=') for pair in connection_string.split(' '))
 
 DATABASES = {
     'default': {
@@ -33,6 +34,3 @@ DATABASES = {
         'PASSWORD': parameters['password'],
     }
 }
-
-DEBUG = False
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
